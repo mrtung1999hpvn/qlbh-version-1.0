620e1a8aa57066fcd5a97c876e8c17473f0b74bc
ThanhTung <63633930+mrtung1999hpvn@users.noreply.github.com>
2020-09-17 17:44:48 +0800

    first commit

---
+++ src/hosting/host.js
@@ -0,0 +1,8 @@
+const nguoidung = "https://qlbh-version-1.herokuapp.com/nguoidung"
+const connect ="https://qlbh-version-1.herokuapp.com/connect"
+
+
+export default {
+    nguoidung,
+    connect
+}
